372ad2a68368b228d3becd5aaccdb71ffcbb1084
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-02-20 13:53:27 +0100

    Opgave 1a

---
+++ series2/opgave1.py
@@ -0,0 +1,20 @@
+import random
+
+def generateGraph(NODES):
+	graph = []
+	
+	random.seed(4321854)
+	
+	for i in range(0, NODES):
+		innerList = []
+		for j in range(0, NODES):
+			if(random.randint(0, 1) == 0):
+				innerList.append(random.randint(1, 100))
+			else:
+				innerList.append(-1);
+
+		graph.append(innerList)
+		
+	return graph
+	
+generateGraph(10)
