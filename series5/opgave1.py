fc1eedaed9337f05ea206ac1b01ac11b1abbfa59
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-03-14 12:04:37 +0100

    S5, needs some work

---
+++ series5/opgave1.py
@@ -0,0 +1,77 @@
+import random;
+
+def egcd(a, b):
+	if b == 0:
+		return (a, 1, 0);
+	else:
+		r = a % b;
+		q = (a - r) / b;
+		(d, k, l) = egcd(b, r);
+		return (d, l, k - l*q);
+		
+def calcKeys(phi):
+	while(True):
+		e = random.getrandbits(16) + 1;
+		(div, d1, d2) = egcd(phi, e);
+		if div == 1:
+			return (e, d1);	
+			
+def strToInt(str):
+	num = 0;
+	for s in str:
+		num |= ord(s);
+		num <<= 8;
+		
+	# Last loop shifts 8 bits extra
+	num >>= 8;
+	return num;
+	
+def intToStr(num):
+	str = "";
+	while num != 0:
+		str = chr(num & 0xFF) + str;
+		num >>= 8;
+		
+	return str;
+	
+def encrypt(msg, e, n):
+	M = strToInt(msg);
+	print "Numberified: " + str(M);
+	print "Back check: " + str((intToStr(M) == msg));
+	
+	# c = M^e mod n
+	return pow(M, e, n);
+
+def decrypt(C, d, n):
+	# m = C^d mod
+	m = pow(C, d, n);
+	print m;
+	return intToStr(m);
+
+random.seed(1);
+	
+p = 991;
+q = 4447;
+
+print "p: " + str(p) + " q: " + str(q);
+
+n = p * q;
+print "n: " + str(n);
+
+phi = (p-1)*(q-1);
+print "phi: " + str(phi);
+
+(e, d) = calcKeys(phi);
+print "e: " + str(e) + " d: " + str(d);
+
+# destroy p, q and phi
+p = q = phi = 0;
+
+msg = "Hi";
+print "Message: " + msg;
+
+C = encrypt(msg, e, n);
+print "Encrypted: " + str(C)
+
+print "Decrypted: " + decrypt(C, d, n);
+
