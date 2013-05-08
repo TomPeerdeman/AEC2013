b4731348f5493dcdb98ac542277c19ac749b7837
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-05-08 17:14:44 +0200

    Half done

--- statred_opgave7.5/ex5.3.py
+++ statred_opgave7.5/ex5.3.py
@@ -9,15 +9,13 @@ d = a[100:126,100:126]
 subplot(1,2,2)
 imshow(d)
 savefig('figures/trui_with_detail.pdf',bbox_inches='tight')
-print(d.shape)
 
-# Dit kan wel eens h,w zijn
-w,h = a.shape
+h, w = a.shape
 X = np.empty((53824, 625), dtype=float)
 
 for x in xrange(0, w-24):
 	for y in xrange(0, h-24):
-		X[:][x*25+y-1] = a[x:(x + 25), y:(y + 25)].reshape(625)
+		X[:][x*25+y-1] = np.reshape(a[x:(x + 25), y:(y + 25)], 625)
 
 # empty vector for m
 m = np.zeros((625),dtype=float)
@@ -25,14 +23,9 @@ e = np.zeros((625,625),dtype=float)
 # calc mean
 for x in X:
 	m += x
-	e += (x*np.transpose(x))
+	e += (np.outer(x, x))
 M = (1.0/shape(X)[0])*m
-S = (e - shape(X)[0]*M*np.transpose(M))/(shape(X)[0]-1)
-
-summ = 0.0
-for me in M:
-	summ += m
-print summ
+S = (e - shape(X)[0]*np.outer(M, M))/(shape(X)[0]-1)
 
 def eigensort(M):
 	d, U = np.linalg.eig(M)
@@ -44,6 +37,5 @@ def eigensort(M):
 d, U = eigensort(S)
 
 figure(2)
-#bar(range(10), d[:10])
 plot(range(10), d[:10])
-savefig('figures/eigen.pdf')
+savefig('figures/eigenvalues.pdf')
