a91538971619a8995353254a3c56c3c93af72ece
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-05-08 13:56:57 +0200

    Weird X shape

--- statred_opgave7.5/ex5.3.py
+++ statred_opgave7.5/ex5.3.py
@@ -1,4 +1,6 @@
 from pylab import *
+import numpy as np
+
 a = imread('data/trui.png')
 figure(1)
 subplot(1,2,1)
@@ -8,3 +10,13 @@ subplot(1,2,2)
 imshow(d)
 savefig('figures/trui_with_detail.pdf',bbox_inches='tight')
 print(d.shape)
+
+# Dit kan wel eens h,w zijn
+w,h = a.shape
+Xl = []
+for x in range(0, w-25):
+	for y in range(0, h-25):
+		Xl.append(np.reshape(a[x:(x + 25), y:(y + 25)], 625));
+X = np.array(Xl)
+X = np.transpose(X)
+print shape(X)	
