bee7672642789728a5725a36e9ba25fef5f8bb2b
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-05-08 16:51:28 +0200

    Thing

--- statred_opgave7.5/ex5.3.py
+++ statred_opgave7.5/ex5.3.py
@@ -13,35 +13,36 @@ print(d.shape)
 
 # Dit kan wel eens h,w zijn
 w,h = a.shape
-X = np.zeros((53824, 625), dtype=float)
-i = 0
-for x in range(0, w-24):
-	for y in range(0, h-24):
-		X[i][:] = np.reshape(a[x:(x + 25), y:(y + 25)], 625)
-		i += 1
+X = np.empty((53824, 625), dtype=float)
+
+for x in xrange(0, w-24):
+	for y in xrange(0, h-24):
+		X[:][x*25+y-1] = a[x:(x + 25), y:(y + 25)].reshape(625)
 
 # empty vector for m
 m = np.zeros((625),dtype=float)
 e = np.zeros((625,625),dtype=float)
 # calc mean
-for x in range(0,shape(X)[1]):
-	m += X[x]
-	e += (X[x]*np.transpose(X[x]))
-M = (1.0/shape(X)[1])*m
-S = (e - shape(X)[1]*M*np.transpose(M))/(shape(X)[1]-1)
-print shape(S)
+for x in X:
+	m += x
+	e += (x*np.transpose(x))
+M = (1.0/shape(X)[0])*m
+S = (e - shape(X)[0]*M*np.transpose(M))/(shape(X)[0]-1)
+
+summ = 0.0
+for me in M:
+	summ += m
+print summ
 
 def eigensort(M):
-	d, U = eig(M)
-	si = argsort(d)[-1::-1]
+	d, U = np.linalg.eig(M)
+	si = np.argsort(d)[-1::-1]
 	d = d[si]
 	U = U[:, si]
 	return (d, U)
 
 d, U = eigensort(S)
-print shape(d)
-print shape(U)
-print d
+
 figure(2)
 #bar(range(10), d[:10])
 plot(range(10), d[:10])
