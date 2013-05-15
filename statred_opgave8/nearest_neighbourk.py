3d339252f8d9d920f27afcb9c65e5e10da7d5919
Rene <rene66613@gmail.com>
2013-05-15 14:37:50 +0200

    test for k=1

--- statred_opgave8/nearest_neighbourk.py
+++ statred_opgave8/nearest_neighbourk.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append("./python")
 
 from nnbk import NNbk
+from nnb import NNb
 
 def cnvt(s):
 	tab = {'Iris-setosa':1.0, 'Iris-versicolor':2.0, 'Iris-virginica':3.0}
@@ -54,4 +55,36 @@ for i in range(4):
 		scatter(XC[T,i], XC[T,j], s=30, marker='+',
 						edgecolor=color[c.astype(int)-1])
 
-savefig('figures/nnbktest.pdf')
+savename = 'figures/nnbk/nnbk_'+ str(k) + '.pdf' 
+savefig(savename)
+
+if k==1:
+	nnc = NNb(X, XC[L,-1])
+	# Classification of Test Set
+	c = zeros(len(T))
+	for i in arange(len(T)):
+		c[i] = nnc.classify(XC[T[i],0:4])
+
+	# Confusion Matrix
+	CM = zeros((3,3))
+	for i in range(3):
+		for j in range(3):
+			CM[i,j] = sum(logical_and(XC[T,4]==(i+1), c==(j+1)))
+
+	print CM
+
+	# Plot Test Set
+	figure(1)
+	color = array([[1,0,0],[0,1,0],[0,0,1]])
+	for i in range(4):
+		for j in range(4):
+			subplot(4,4,4*i+j+1)
+			if i==j:
+				continue
+			scatter(XC[T,i], XC[T,j], s=100, marker='s',
+							edgecolor=color[XC[T,4].astype(int)-1],
+							facecolor=[1,1,1]*len(T))
+			scatter(XC[T,i], XC[T,j], s=30, marker='+',
+							edgecolor=color[c.astype(int)-1])
+
+	savefig('figures/nnbk/nnb.pdf')
