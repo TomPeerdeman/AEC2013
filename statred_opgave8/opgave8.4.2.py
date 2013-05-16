61871dcc889663233433382e8a879f786a5879bc
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-05-16 11:53:09 +0200

    Blaa

--- statred_opgave8/opgave8.4.2.py
+++ statred_opgave8/opgave8.4.2.py
@@ -2,12 +2,19 @@ from scipy.stats import norm
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0,15,100)
-y1 = norm.pdf(x, 4, 1)
+x = np.linspace(-5,15,200)
+y1 = norm.pdf(x, 4, 2.5)
 y2 = norm.pdf(x, 7, 2)
 
 plt.figure(1)
-plt.plot(x, y1)
-plt.plot(x, y2)
+plt.plot(x, y1, color="blue")
+plt.plot(x, y2, color="red")
+
+px = norm.pdf(x, 4, 1) * 0.3 + norm.pdf(x, 7, 1.5) * 0.7
+P1x = norm.pdf(x, 4, 1) * 0.3 / px
+P2x = norm.pdf(x, 7, 1.5) * 0.7 / px
+
+plt.plot(x, P1x, linestyle="dashed", color="blue")
+plt.plot(x, P2x, linestyle="dashed", color="red")
 plt.show()
 
