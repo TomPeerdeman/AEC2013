5538566af492035100a8284b31956a1864917a0d
René Aparicio Saez <10214054@u019918.fnwi.uva.nl>
2013-05-16 12:05:57 +0200

    yay plots

--- statred_opgave8/opgave8.4.2.py
+++ statred_opgave8/opgave8.4.2.py
@@ -3,18 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(-5,15,200)
-y1 = norm.pdf(x, 4, 2.5)
-y2 = norm.pdf(x, 7, 2)
+y1 = norm.pdf(x, 4, 1)*0.3
+y2 = norm.pdf(x, 7, 1.5)*0.7
 
 plt.figure(1)
-plt.plot(x, y1, color="blue")
-plt.plot(x, y2, color="red")
 
-px = norm.pdf(x, 4, 1) * 0.3 + norm.pdf(x, 7, 1.5) * 0.7
-P1x = norm.pdf(x, 4, 1) * 0.3 / px
-P2x = norm.pdf(x, 7, 1.5) * 0.7 / px
+p1, = plt.plot(x, y1, color="blue")
+p2, = plt.plot(x, y2, color="red")
 
-plt.plot(x, P1x, linestyle="dashed", color="blue")
-plt.plot(x, P2x, linestyle="dashed", color="red")
+px = y1 + y2
+P1x = y1 / px
+P2x = y2 / px
+
+p3, = plt.plot(x, P1x, linestyle="dashed", color="blue")
+p4, = plt.plot(x, P2x, linestyle="dashed", color="red")
+
+plt.legend([p3, p4, p1, p2], ["P(C=1|x)", "P(C=2|x)", "pxc(x,C=1)", "pxc(x,C=2)"])
+
 plt.show()
 
