5ed64a6c95d7b87793aefdd7ad411928efb9e6fd
Rene <rene66613@gmail.com>
2013-05-17 14:40:21 +0200

    semi multi pdf

--- statred_opgave8/map.py
+++ statred_opgave8/map.py
@@ -1,5 +1,11 @@
 from pylab import tile, sum, argmin, argmax, amax, mean, cov, zeros, where, empty
+from scipy.stats import norm
 import operator
+import numpy as np
+
+def multivariate_pdf(vector, mean, cov):
+	quadratic_form = np.dot(np.dot(vector-mean,np.linalg.inv(cov)),np.transpose(vector-mean))
+	return np.exp(-.5 * quadratic_form)/ (2*np.pi * np.linalg.det(cov))
 
 class MAP:
 	def __init__(self, X, c):
@@ -22,5 +28,15 @@ class MAP:
 			# Tile smeert mu uit zodat we mu kunnen aftrekken van de X matrix
 			self.cov[i] = cov(Xa - tile(self.mu[i].T, len(Xa[0])).reshape(4, len(Xa[0])))
 
+		print self.mu
+		print self.cov
+	
 	def classify(self, x):
-		return 0
+		y1 = multivariate_pdf(x, self.mu[0], self.cov[0])*(1./3.)
+		y2 = multivariate_pdf(x, self.mu[1], self.cov[1])*(1./3.)
+		y3 = multivariate_pdf(x, self.mu[2], self.cov[2])*(1./3.)
+		px = y1 + y2 + y3
+		P1x = y1 / px
+		P2x = y2 / px
+		P3x = y3 / px
+		return argmax([P1x, P2x, P3x])
