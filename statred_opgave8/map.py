72a3349a10c147304ebdff97ed54957f493f57a1
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-05-17 14:41:01 +0200

    Chance of a class

--- statred_opgave8/map.py
+++ statred_opgave8/map.py
@@ -1,4 +1,4 @@
-from pylab import tile, sum, argmin, argmax, amax, mean, cov, zeros, where, empty
+from pylab import tile, sum, argmin, argmax, amax, mean, cov, zeros, where, empty, shape
 import operator
 
 class MAP:
@@ -7,8 +7,9 @@ class MAP:
 		self.X = X
 		self.c = c
 		
-		self.mu = empty((3, 4))
-		self.cov = empty((3, 4, 4))
+		self.mu = empty((3, self.n))
+		self.cov = empty((3, self.n, self.n))
+		self.P = empty(3)
 		cond = zeros(self.N)
 		for i in range(0, 3):
 			cond = cond + 1.0
@@ -17,10 +18,14 @@ class MAP:
 			Xa = [X[:,b] for b in indices]
 			# Bovenstaande pakt de xjes in een extra array, dit willen we niet
 			Xa = Xa[0]
+			Na = shape(Xa)[1]
 
 			self.mu[i] = mean(Xa, axis=1)
 			# Tile smeert mu uit zodat we mu kunnen aftrekken van de X matrix
-			self.cov[i] = cov(Xa - tile(self.mu[i].T, len(Xa[0])).reshape(4, len(Xa[0])))
+			self.cov[i] = cov(Xa - tile(self.mu[i].T, Na).reshape(self.n, Na))
+
+			# De kans op deze klasse
+			self.P[i] = (Na * 1.0) / self.N
 
 	def classify(self, x):
 		return 0
