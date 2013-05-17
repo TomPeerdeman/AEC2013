6978ca968613b9cab163807032bc5b71c9f92151
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-05-17 13:47:32 +0200

    Split classes

--- statred_opgave8/map.py
+++ statred_opgave8/map.py
@@ -1,4 +1,4 @@
-from pylab import tile, sum, argmin, array, argmax, amax, mean, cov
+from pylab import tile, sum, argmin, array, argmax, amax, mean, cov, zeros, where
 import operator
 
 class MAP:
@@ -7,10 +7,20 @@ class MAP:
 		self.X = X
 		self.c = c
 		
-				
+		#mu = array(3, 4)
+		#cov = array(3, self.)
+		cond = zeros(self.N)
+		for i in range(0, 3):
+			cond = cond + 1.0
+			indices = where(self.c==cond)
+			print "Indices: " + str(cond[0])
+			print indices
+			Xa = [X[:,b] for b in indices]
+			print "X' :"
+			print Xa
 
-		self.mu = mean(X, axis=0)
-		self.cov = cov(X - self.mu)
+		#self.mu = mean(X, axis=0)
+		#self.cov = cov(X - self.mu)
 
 	def classify(self, x):
 		return 0
