58050f69cdb114ab7b98bc4fb00dad1add08a061
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-05-22 14:09:31 +0200

    PCA x2 ?

--- statred_opgave9/pca.py
+++ statred_opgave9/pca.py
@@ -1,16 +1,23 @@
 import numpy as np
 import numpy.linalg as la
 
+# Original code found from 'Handout_5_PCA.pdf' page 4
+def eigensort(M):
+	d, U = la.eig(M)
+	si = np.argsort(d)[-1::-1]
+	d = d[si]
+	U = U[:, si]
+	return (d, U)
+
+# Inpiration: http://stackoverflow.com/questions/13224362/pca-analysis-with-python
+#           & exercise 7.5.3
 def pca(X, k):
 	M = np.mean(X, axis = 0)
 	X -= M
 	S = np.cov(np.transpose(X))
 	
-	d, U = la.eig(S)
-	si = np.argsort(d)[-1::-1]
-	d = d[si]
-	U = U[:, si]
+	d, U = eigensort(S)
+	U = U[:,:k];
 
-	Y = np.dot(np.transpose(U), X)
-	return np.dot(U[:,:k], Y[:k]) + M
+	return np.transpose(np.dot(np.transpose(U), np.transpose(X)))
 
