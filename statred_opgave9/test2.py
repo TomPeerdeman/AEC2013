8f1dc58173f848572b08117e6aa8abc5a7a650c2
Tom Peerdeman <tompeerdeman93@hotmail.com>
2013-05-22 19:39:25 +0200

    Test set used

--- statred_opgave9/test2.py
+++ statred_opgave9/test2.py
@@ -30,13 +30,11 @@ for i in range(0,len(lines),2):
 		y = append(y,ind)
 
 U, X = pca.pca(D, 3)
-# TODO: X needs scaling to [0-1]
 
+# Scale values to [0-1]
 minval = X.min()
 maxval = X.max()
 X = (X - minval)/(maxval-minval)
-
-classes = arange(len(Colors))
 
 ind = arange(len(X))
 ind = permutation(ind)
@@ -91,13 +89,35 @@ for c in range(-3, 16, 2):
 			bestG = gamma
 			bestperc = perc
 			bestparam = param		
-		# TODO: Test accuracy using svm_predict & save result?
+
+# Test on testing set
+prob = svm_problem(y[L].tolist(), X[L].tolist())
+param = svm_parameter('-q -c ' + str(2**bestC) + ' -g ' + str(2**bestG))
+svm = svm_train(prob, param)
+confusion=zeros((len(Colors),len(Colors)))
+
+for i in T:
+	predictedClass, notused1, notused2 = svm_predict([0], [X[i].tolist()], svm);
+	confusion[y[i], predictedClass[0]]+=1
+
 # Show the results to the user
 print '\n~ ~ ~ ~ ~ ~'
-print 'Best parameters: '
-print bestparam
-print "\n" + str(2**bestC) + "," + str(2**bestG)
-print 'Best Accuracy'
+print 'Best parameters: (c, gamma)'
+print str(2**bestC) + ", " + str(2**bestG)
+print 'Best v-fold accuracy'
 print str(perc) + '%'
 
+# Calculate the accuracy of the SVM
+totalmiss= 0.0;
+total = 0.0;
+for i in range(-1,len(Colors)):
+	for j in range(-1, len(Colors)):
+		if i != j:
+			totalmiss+=confusion[i,j]
+		total += confusion[i,j]
+perc = totalmiss / total * 100.0;
+perc = 100.0 - perc;
+
+print 'Testing set accuracy'
+print str(perc) + '%'
 
